49e19d360a0d3cb300da3c0c3f45a6f0e236c602
John Atkins <johnatkins1999@yahoo.com>
2019-11-23 22:27:35 -0500

    states

--- src/Main/statesActions.js
+++ src/Main/statesActions.js
@@ -5,6 +5,7 @@ import { GET_HEADER, URL } from '../constants/constants.js';
 export const UPDATE_STATES='UPDATE_STATES';
 
 export function getStates(){
+  console.log('URL',URL);
   return dispatch => {
     fetch(`${URL}locations/states`, GET_HEADER)
     .then(response => response.json())
