34775f56ef3075edcc2a69756937c5e6123e96e2
John Atkins <johnatkins1999@yahoo.com>
2020-01-20 13:04:22 -0500

    search display fix

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
+import SanitizedHTML from 'react-sanitized-html';
 
 const moment = require('moment');
 
@@ -20,7 +21,7 @@ class Results extends Component {
                           {this.props.item.city}, {this.props.item.abbr} ({parseInt(this.props.item.distance)} miles)
 
                           { (this.props.prod.length > 0)
-                              ? <div className="upcoming"><span className="runin next">Next Production:</span> <span className="prod">{ this.props.prod[0][0].title }</span>&emsp;({moment.utc(this.props.prod[0][0].start_date).format('M/D/YY')}&ndash;{moment(this.props.prod[0][0].end_date).format('M/D/YY')})</div>
+                              ? <div className="upcoming"><span className="runin next">Next Production:</span> <span className="prod"><SanitizedHTML html={ this.props.prod[0][0].title }/></span>&emsp;({moment.utc(this.props.prod[0][0].start_date).format('M/D/YY')}&ndash;{moment.utc(this.props.prod[0][0].end_date).format('M/D/YY')})</div>
                               : <div className="upcoming"><span className="noresults">No Upcoming Productions Available</span></div>
                           }
 
@@ -31,7 +32,7 @@ class Results extends Component {
                     { (this.props.type==='2')
                       ? <span>
                           <span className="theater_name">
-                            <Link className='website' to={`/theater/${this.props.item.id}`}>{this.props.item.name}</Link>, {this.props.item.city}, {this.props.item.abbr}
+                            <Link className='website' to={`/theater/${this.props.item.id}`}><SanitizedHTML html={this.props.item.name}/></Link>, {this.props.item.city}, {this.props.item.abbr}
                           </span>
                         </span>
                       : null
@@ -40,7 +41,7 @@ class Results extends Component {
                     { (this.props.type==='3')
                       ? <span>
                           <span className="show_title">
-                            <Link className='website' to={`/prodsbyshow/${this.props.item.id}`}>{this.props.item.title}</Link>
+                            <Link className='website' to={`/prodsbyshow/${this.props.item.id}`}><SanitizedHTML html={this.props.item.title} /></Link>
                           </span>
                         </span>
                         : null
