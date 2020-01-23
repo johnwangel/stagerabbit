c58938114b932133d64f634b0fb14ad7fc50bcf7
John Atkins <johnatkins1999@yahoo.com>
2020-01-23 12:11:05 -0500

    login fix

--- src/Register/reducers.js
+++ src/Register/reducers.js
@@ -1,7 +1,7 @@
-import { REGISTER, LOGIN, LOGOUT } from './actions';
+import { REGISTER, LOGIN, LOGOUT, LOGINERROR } from './actions';
 import React from 'react';
 
-const User = ( state = { level: 1, name: 'guest', token: null, loggedin: false, tid: null }, action ) => {
+const User = ( state = { level: 1, name: 'guest', token: null, loggedin: false, tid: null, message: null }, action ) => {
   switch (action.type){
     case REGISTER:
       return register(state,action);
@@ -9,6 +9,8 @@ const User = ( state = { level: 1, name: 'guest', token: null, loggedin: false, 
       return login(state,action);
     case LOGOUT:
       return logout(state,action);
+    case LOGINERROR:
+      return loginerr(state,action);
     default:
       return state;
   }
@@ -19,7 +21,7 @@ function register(state,action){
   let name=`${user.fname} ${user.lname}`;
   let token=(user.token !=='')?user.token : null;
   let tid=user.tid;
-  return { level: user.level, name: name, token: token, loggedin: true, tid: tid } ;
+  return { level: user.level, name: name, token: token, loggedin: true, tid: tid, message: null } ;
 }
 
 function login(state,action){
@@ -27,13 +29,18 @@ function login(state,action){
   let name=`${user.fname} ${user.lname}`;
   let token=(user.token !=='')?user.token : null;
   let tid=user.tid;
-  let item={ level: user.level, name: name, token: token, loggedin: true, tid: tid };
-  console.log('item in login',item)
+  let item={ level: user.level, name: name, token: token, loggedin: true, tid: tid, message: null };
   return item;
 }
 
+function loginerr(state,action){
+  const newstate = Object.assign({}, state);
+  newstate.message = action.payload.message;
+  return newstate;
+}
+
 function logout(state,action){
-  return { level: 1, name: 'guest', token: null, loggedin: false, tid: null } ;
+  return { level: 1, name: 'guest', token: null, loggedin: false, tid: null, message: null } ;
 }
 
 export default User;
