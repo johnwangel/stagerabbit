1169c744c95c3b6892e54ec29fb38acf679adac2
John Atkins <johnatkins1999@yahoo.com>
2020-02-12 17:11:27 -0500

    login page

--- src/Productions/prodsByShow.js
+++ src/Productions/prodsByShow.js
@@ -13,39 +13,61 @@ class ProdByShow extends Component {
   constructor(props) {
     super(props);
     this.props.prodsByShow(this.props.match.params.id);
+    this.state = { show_prods:0 };
+    this.toggleProds=this.toggleProds.bind(this);
+  }
+
+  toggleProds(number){
+    this.setState({show_prods:number});
   }
 
   render() {
-    console.log('prods',this.props)
     return ( <div className='show_results'>
-              <h2>Results</h2>
-                { ( this.props.Prods && this.props.Prods.upcoming && this.props.Prods.upcoming.length>0)
-                  ? <div>
-                      <h3>Upcoming Productions</h3>
-                      <ol>
-                        { this.props.Prods.upcoming.map((item, idx) => {
-                              return <li>
-                                  <span className='title'><SanitizedHTML html={ item.title } /></span>, <Link className='website' to={`/theater/${item.theater_id}`}>
-                                  <SanitizedHTML html={item.theater_name} /></Link>,&nbsp;<SanitizedHTML html={ item.city_name} />,&nbsp;{ item.state_abbr }
-                                  <span className="dates">({moment.utc(item.start_date).format('M/D/YY')}&ndash;{moment.utc(item.end_date).format('M/D/YY')})</span></li>
-                            })
-                        }
-                      </ol>
-                    </div>
-                  : null
+              <h2 className="main-page main-column search">Production Results</h2>
+              <div className="toggle main-column">
+                <div  id="upcoming1"
+                      className={ (this.state.show_prods===0) ? "toggle-button active" : "toggle-button" }
+                      onClick={() => { this.toggleProds(0) } }>
+                    Upcoming
+                </div>
+                <div  id="previous1"
+                      className={ (this.state.show_prods===1) ? "toggle-button active" : "toggle-button" }
+                      onClick={() => { this.toggleProds(1) } }>
+                    Past
+                </div>
+              </div>
+              { ( this.props.Prods && this.props.Prods.upcoming && this.props.Prods.upcoming.length>0)
+                ? <div className={ (this.state.show_prods===0) ? 'searchContent main-column' : 'searchContent main-column hide' } >
+                      { this.props.Prods.upcoming.map((item, idx) => {
+                            return <div className="result-item">
+                                      <div className='number'>{idx+1}</div>
+                                      <div className='title'>
+                                        <SanitizedHTML html={ item.title } />
+                                      </div>
+                                      <Link className='website' to={`/theater/${item.theater_id}`}>
+                                        <SanitizedHTML html={item.theater_name} />
+                                      </Link><SanitizedHTML html={ `${item.city_name}, ${item.state_abbr}` } />
+                                      <div className="dates">({moment.utc(item.start_date).format('M/D/YY')}&ndash;{moment.utc(item.end_date).format('M/D/YY')})</div>
+                                  </div>
+                          })
+                      }
+                  </div>
+                : null
                 }
                 { ( this.props.Prods && this.props.Prods.previous && this.props.Prods.previous.length>0)
-                  ? <div>
-                      <h3>Past Productions</h3>
-                      <ol>
+                  ? <div className={ (this.state.show_prods===1) ? 'searchContent main-column' : 'searchContent main-column hide' } >
                         { this.props.Prods.previous.map((item, idx) => {
-                              return <li>
-                                  <span className='title'><SanitizedHTML html={ item.title } /></span>, <Link className='website' to={`/theater/${item.theater_id}`}>
-                                  <SanitizedHTML html={item.theater_name} /></Link>,&nbsp;<SanitizedHTML html={ item.city_name} />,&nbsp;{ item.state_abbr }
-                                  <span className="dates">({moment.utc(item.start_date).format('M/D/YY')}&ndash;{moment.utc(item.end_date).format('M/D/YY')})</span></li>
-                            })
+                              return <div className="result-item">
+                                      <div className='number'>{idx+1}</div>
+                                      <div className='title'>
+                                        <SanitizedHTML html={ item.title } />
+                                      </div>
+                                      <Link className='website' to={`/theater/${item.theater_id}`}>
+                                        <SanitizedHTML html={item.theater_name} />
+                                      </Link><SanitizedHTML html={ `${item.city_name}, ${item.state_abbr}` } />
+                                      <div className="dates">({moment.utc(item.start_date).format('M/D/YY')}&ndash;{moment.utc(item.end_date).format('M/D/YY')})</div>
+                                  </div>                            })
                         }
-                      </ol>
                     </div>
                   : null
                 }
