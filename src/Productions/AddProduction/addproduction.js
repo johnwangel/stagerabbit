310e6874980fc8f4366b44c9f080320747a689a3
John Atkins <johnatkins1999@yahoo.com>
2020-02-16 19:16:57 -0500

    expandable details in add prod

--- src/Productions/AddProduction/addproduction.js
+++ src/Productions/AddProduction/addproduction.js
@@ -49,14 +49,20 @@ class AddProd extends Component {
       cast: (p && p.cast) ? p.cast : '',
       description: (p && p.description) ? p.description : '',
       height: null,
-      scroll: null
+      scroll: null,
+      expand_desc: false,
+      expand_cast: false,
+      expand_dir: false,
+      expand_chor: false,
+      expand_md: false
     };
 
     this.handleNew = this.handleNew.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
     this.handleStartDateChange = this.handleStartDateChange.bind(this);
     this.handleEndDateChange = this.handleEndDateChange.bind(this);
+    this.handleExpand = this.handleExpand.bind(this)
     this.onDropdownSelected = this.onDropdownSelected.bind(this);
     this.scrollToMyRef = this.scrollToMyRef.bind(this);
   }
@@ -106,6 +112,28 @@ class AddProd extends Component {
 
   handleEndDateChange( date ) {
     this.setState({ end : date } );
+  }
+
+  handleExpand(id){
+    switch (id){
+      case 1:
+        this.setState({ expand_desc: !this.state.expand_desc });
+        break;
+      case 2:
+        this.setState({ expand_cast: !this.state.expand_cast });
+        break;
+      case 3:
+        this.setState({ expand_dir: !this.state.expand_dir });
+        break;
+      case 4:
+        this.setState({ expand_chor: !this.state.expand_chor });
+        break;
+      case 5:
+        this.setState({ expand_md: !this.state.expand_md });
+        break;
+      default:
+        return;
+    }
   }
 
   onDropdownSelected( e ) {
@@ -174,106 +202,125 @@ class AddProd extends Component {
             </div>
 
             <div className='form-group'>
-              <div className="label">Description:</div>
-              <textarea
-                    id="description_1"
-                    name="description"
-                    value={this.state.description}
-                    onChange={this.handleChange} />
+              <h4 onClick={ () => { this.handleExpand(1) } } className="form-title expand">
+                <span id="desc-show" className="plus">{ (this.state.expand_desc) ? '-' : '+' }</span>Description:
+              </h4>
+              <div id="desc-display" className={ (this.state.expand_desc) ? "add_artist show" : "add_artist" }>
+                <textarea
+                      id="description_1"
+                      name="description"
+                      value={this.state.description}
+                      onChange={this.handleChange} />
+              </div>
             </div>
 
             <div className='form-group'>
-              <div className="label">Cast:</div>
-              <textarea
-                  id="cast_1"
-                  name="cast"
-                  value={this.state.cast}
-                  onChange={this.handleChange} />
+              <h4 onClick={ () => { this.handleExpand(2) } } className="form-title expand">
+                <span id="cast-show" className="plus">{ (this.state.expand_cast) ? '-' : '+' }</span>Cast
+              </h4>
+              <div id="desc-display" className={ (this.state.expand_cast) ? "add_artist show" : "add_artist" }>
+                <textarea
+                    id="cast_1"
+                    name="cast"
+                    value={this.state.cast}
+                    onChange={this.handleChange} />
+              </div>
             </div>
 
             <div id="dir-group">
-              <h3 className='form-title'>Director:</h3>
-              {
-                [...Array(this.state.dirChildren)].map( (m,i,a) =>{
-                  return <AddArtist
-                              key={`d-${i}`}
-                              num={i+1}
-                              pid={ this.props.production }
-                              type="dir"
-                              title="Director"
-                              editmode={this.state.editmode}
-                              item={(this.state.dir_items) ? this.state.dir_items[i] : null}
-                              sel={this.props.artists}
-                              addArtistCB={ this.props.addArtistCB }
-                              removeArtistCB={ this.props.removeArtistCB }
-                              newArtist={ this.props.newArtist }
-                            />
-                })
-              }
-            </div>
+              <h4 onClick={ () => { this.handleExpand(3) } } className='form-title expand'>
+                <span id="dir-show" className="plus">{ (this.state.expand_dir) ? '-' : '+' }</span>Director
+              </h4>
+              <div id="dir-display" className={ (this.state.expand_dir) ? "add_artist show" : "add_artist" }>
+                {
+                  [...Array(this.state.dirChildren)].map( (m,i,a) =>{
+                    return <AddArtist
+                                key={`d-${i}`}
+                                num={i+1}
+                                pid={ this.props.production }
+                                type="dir"
+                                title="Director"
+                                editmode={this.state.editmode}
+                                item={(this.state.dir_items) ? this.state.dir_items[i] : null}
+                                sel={this.props.artists}
+                                addArtistCB={ this.props.addArtistCB }
+                                removeArtistCB={ this.props.removeArtistCB }
+                                newArtist={ this.props.newArtist }
+                              />
+                  })
+                }
 
-            <div className='another-artist'
-                 id="dirChildren"
-                 onClick={this.handleNew}>
-              <span className="plus">+</span>
-              Another Director
+                <div className='another-artist'
+                     id="dirChildren"
+                     onClick={this.handleNew}>
+                  <span className="plus">+</span>
+                  Another Director
+                </div>
+              </div>
             </div>
 
             <div id="chor-group">
-              <h3 className='form-title'>Choreographer:</h3>
-              {
-                [...Array(this.state.chorChildren)].map( (m,i,a) =>{
-                  return <AddArtist
-                              key={`c-${i}`}
-                              num={i+1}
-                              pid={ this.props.production }
-                              editmode={this.state.editmode}
-                              type="chor"
-                              title="Choreographer"
-                              item={(this.state.chor_items) ? this.state.chor_items[i] : null}
-                              sel={this.props.artists}
-                              addArtistCB={ this.props.addArtistCB }
-                              removeArtistCB={ this.props.removeArtistCB }
-                              newArtist={ this.props.newArtist }
-                            />
-                })
-              }
+              <h4 onClick={  () => { this.handleExpand(4) } } className='form-title expand'>
+                <span id="dir-show" className="plus">{ (this.state.expand_chor) ? '-' : '+' }</span>Choreographer
+              </h4>
+              <div id="chor-display" className={ (this.state.expand_chor) ? "add_artist show" : "add_artist" }>
+                {
+                  [...Array(this.state.chorChildren)].map( (m,i,a) =>{
+                    return <AddArtist
+                                key={`c-${i}`}
+                                num={i+1}
+                                pid={ this.props.production }
+                                editmode={this.state.editmode}
+                                type="chor"
+                                title="Choreographer"
+                                item={(this.state.chor_items) ? this.state.chor_items[i] : null}
+                                sel={this.props.artists}
+                                addArtistCB={ this.props.addArtistCB }
+                                removeArtistCB={ this.props.removeArtistCB }
+                                newArtist={ this.props.newArtist }
+                              />
+                  })
+                }
+                <div className='another-artist'
+                     id="chorChildren"
+                     onClick={this.handleNew}>
+                  <span className="plus">+</span>
+                  Another Choreograher
+                </div>
+              </div>
             </div>
-
-            <div className='another-artist'
-                 id="chorChildren"
-                 onClick={this.handleNew}>
-              <span className="plus">+</span>
-              Another Choreograher
-            </div>
-
 
             <div id="md-group">
-              <h3 className='form-title'>Music Director:</h3>
-              {
-                [...Array(this.state.mdChildren)].map( (m,i,a) =>{
-                  return <AddArtist
-                              key={`md-${i}`}
-                              num={i+1}
-                              pid={ this.props.production }
-                              type="md"
-                              title="Music Director"
-                              sel={this.props.artists}
-                              editmode={this.state.editmode}
-                              item={(this.state.md_items) ? this.state.md_items[i] : null}
-                              addArtistCB={ this.props.addArtistCB }
-                              removeArtistCB={ this.props.removeArtistCB }
-                              newArtist={ this.props.newArtist }
-                            />
-                })
-              }
+              <h4 onClick={ () => { this.handleExpand(5) } }  className='form-title expand'>
+                  <span id="dir-show" className="plus">{ (this.state.expand_md) ? '-' : '+' }</span>Music Director
+              </h4>
+              <div id="md-display" className={ (this.state.expand_md) ? "add_artist show" : "add_artist" }>
+                {
+                  [...Array(this.state.mdChildren)].map( (m,i,a) =>{
+                    return <AddArtist
+                                key={`md-${i}`}
+                                num={i+1}
+                                pid={ this.props.production }
+                                type="md"
+                                title="Music Director"
+                                sel={this.props.artists}
+                                editmode={this.state.editmode}
+                                item={(this.state.md_items) ? this.state.md_items[i] : null}
+                                addArtistCB={ this.props.addArtistCB }
+                                removeArtistCB={ this.props.removeArtistCB }
+                                newArtist={ this.props.newArtist }
+                              />
+                  })
+                }
+
+              <div className='another-artist'
+                   id="mdChildren"
+                   onClick={this.handleNew}>
+                <span className="plus">+</span>
+                Another MD
+              </div>
             </div>
-            <div className='another-artist'
-                 id="mdChildren"
-                 onClick={this.handleNew}>
-              <span className="plus">+</span>
-              Another MD
-            </div>
+          </div>
 
             <input
                 className='form-button'
